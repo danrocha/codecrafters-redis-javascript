ec12a5a404cb144b31ae8df312b058052f6c2a22
Daniel da Rocha <daniel.darocha@birdeatsbug.com>
2022-12-17 11:23:35 +0100

    respond to ping with pong

--- app/main.js
+++ app/main.js
@@ -6,6 +6,7 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
+  connection.write("+PONG\r\n");
 });
 
 server.listen(6379, "127.0.0.1");
