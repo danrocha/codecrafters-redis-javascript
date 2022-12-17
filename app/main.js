9026fcc49c32fbed4d3b2f05c909feb021cecdc1
Daniel da Rocha <daniel.darocha@birdeatsbug.com>
2022-12-17 11:32:41 +0100

    add listener for data

--- app/main.js
+++ app/main.js
@@ -6,7 +6,9 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
-  connection.write("+PONG\r\n");
+  connection.on("data", (data) => {
+    connection.write("+PONG\r\n");
+  });
 });
 
 server.listen(6379, "127.0.0.1");
